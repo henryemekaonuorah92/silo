f3b9d1cdbd5ceb1cddb393e5c70bd5b9c70246cb
David Moreau <david.moreau@frankandoak.com>
2016-12-29 13:17:46 -0500

    upload modal bind

--- client/Editor/UploadModal.js
+++ client/Editor/UploadModal.js
@@ -1,6 +1,7 @@
 ;
 import React from 'react';
 import {Button, Modal} from 'react-bootstrap';
+import UploadField from './UploadField';
 
 module.exports = React.createClass({
     getInitialState() {
@@ -19,6 +20,10 @@ module.exports = React.createClass({
         this.setState({ showModal: true });
     },
 
+    handleSuccess() {
+        console.log("yop");
+    },
+
     render() {
         return (
             <a onClick={this.open}>
@@ -26,10 +31,13 @@ module.exports = React.createClass({
 
                 <Modal show={this.state.showModal} onHide={this.close}>
                     <Modal.Header closeButton>
-                        <Modal.Title>Modal heading</Modal.Title>
+                        <Modal.Title>CSV Upload</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                         <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+
+                        <UploadField url="arf" onSuccess={this.handleSuccess} />
+
                     </Modal.Body>
                     <Modal.Footer>
                         <Button onClick={this.close}>Close</Button>
