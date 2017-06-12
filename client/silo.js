ef120d3fafa40b0d8cca41ebe0834acf4e6c9b3f
David Moreau <dav.m85@gmail.com>
2017-06-12 16:30:14 -0400

    more silo-core exports

--- client/silo.js
+++ client/silo.js
@@ -1,5 +1,7 @@
 module.exports = {
     Alerts: require('./Common/Alerts'),
     Link: require('./Factory').Link,
-    AjaxButton: require('./Common/AjaxButton')
+    AjaxButton: require('./Common/AjaxButton'),
+    PromiseButton: require('./Common/PromiseButton'),
+    Api: require('./Api')
 };
