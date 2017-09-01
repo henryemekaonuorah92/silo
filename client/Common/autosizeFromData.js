1dd26e87d97a0421ea1d1046df42e05e62beffd8
Andre Siviero <andre.siviero@frankandoak.com>
2017-09-01 11:49:02 -0400

    Restore autosize

--- client/Common/autosizeFromData.js
+++ client/Common/autosizeFromData.js
@@ -1,15 +1,12 @@
 const React = require('react');
 
 module.exports = (WrappedComponent)=>{
-    return ({children, data, ...props}) => {
-        const rowHeight = "rowHeight" in props ? props.rowHeight : 36;
-        return <WrappedComponent
-                    height={Math.min(data.length + 3, 12) * rowHeight}
-                    headerHeight={30}
-                    offsetHeight={150}
-                    rowsCount={data.length}
-                    rowHeight={rowHeight}
-                    {...props}>{children}</WrappedComponent>
-        ;
-    }
+    return ({children, data, ...props}) => <WrappedComponent
+                height={Math.min(data.length + 3, 12) * 36}
+                headerHeight={30}
+                offsetHeight={150}
+                rowsCount={data.length}
+                rowHeight={36}
+                {...props}>{children}</WrappedComponent>
+    ;
 };
