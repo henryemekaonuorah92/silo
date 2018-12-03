8642fbfffcabe61b82b4cffbc5521646bfdfda97
Andre Siviero <andre.siviero@frankandoak.com>
2018-12-03 14:08:58 -0500

    Remove name handling from ProductCell

--- client/Editor/Cell/ProductCell.js
+++ client/Editor/Cell/ProductCell.js
@@ -9,9 +9,6 @@ module.exports = ({rowIndex, data, col, ...props}) => {
     let obj = data.getObjectAt(rowIndex);
     let code = obj.product;
     let anchor = code;
-    if(("name" in obj) && obj.name) {
-        anchor = "("+obj.name.replace(code, '')+") "+code;
-    }
     return (
         <Cell {...props}>
             <Link route="product" code={code}>{anchor}</Link>
