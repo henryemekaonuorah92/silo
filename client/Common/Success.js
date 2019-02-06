840a3451e004ed5a7853e7407b4559cf4f5dbb44
Andre Siviero <andre.siviero@frankandoak.com>
2019-02-06 15:15:00 -0500

    Fix parent assign to allow partial updates

--- client/Common/Success.js
+++ client/Common/Success.js
@@ -9,15 +9,24 @@ module.exports = React.createClass({
 
     getDefaultProps: function() {
         return {
-            onAck: function(){}
+            onAck: function(){},
+            description: null
         };
     },
 
     render: function(){
+        let {description} = this.props
+        console.log(description)
+        if(Array.isArray(description) && description.length) {
+            description = <ul>
+                {description.map((msg) => {return <li>{msg}</li>})}
+            </ul>
+        }
         return (
             <div className="text-center">
                 <span style={{fontSize: "50px"}} className="glyphicon glyphicon-ok" />
                 {this.props.title && <h3>{this.props.title}</h3>}
+                {description && <p>{description}</p>}
                 <button className="btn btn-block btn-success" onClick={this.props.onAck}>Continue</button>
             </div>
         );
--- client/View/LocationMove.js
+++ client/View/LocationMove.js
@@ -5,7 +5,7 @@ const Success = require('../Common/Success');
 const Error = require('../Common/Error');
 const Pending = require('../Common/Pending');
 const CloseButton = require('../Common/CloseButton');
-const {Row, Col, Well} = require('react-bootstrap');
+const {Row, Col, Well, Checkbox} = require('react-bootstrap');
 const MountHandheldScanner = require('../Common/Scanner')
 
 module.exports = React.createClass({
@@ -15,6 +15,7 @@ module.exports = React.createClass({
         error: false,
         parent: null,
         children: [],
+        forceEmpty: false,
     }),
 
     propTypes: {
@@ -88,7 +89,7 @@ module.exports = React.createClass({
             <div>
                 <MountHandheldScanner onScan={this.handleScan} />
                 {this.state.confirmation &&
-                    <Success title="Parent assigned" onAck={this.clearConfirmation} />
+                    <Success title="Parent assigned" description={this.state.confirmation} onAck={this.clearConfirmation} />
                 }
                 {this.state.error &&
                     <Error title="Failure" description={this.state.error} onAck={this.clearConfirmation} />
@@ -127,15 +128,27 @@ module.exports = React.createClass({
                                 })}
 
                                 <p>Scan more children</p>
+
+                                <Checkbox value={this.state.forceEmpty} onClick={()=>{
+                                        let forceEmpty = !this.state.forceEmpty
+                                        this.setState({forceEmpty})
+                                    }}>
+                                    Force assignment in empty locations
+                                </Checkbox>
+
                                 <AjaxButton
                                     url={this.props.siloBasePath + "/inventory/location/" + this.state.parent + "/child"}
                                     type="PATCH"
                                     ref={ref => this.button = ref}
                                     contentType="application/json"
-                                    data={JSON.stringify(this.state.children.slice())}
-                                    onSuccess={() => {
+                                    data={JSON.stringify({children:this.state.children.slice(),forceEmpty:this.state.forceEmpty})}
+                                    onSuccess={(d) => {
+                                        let success = d.operations.map((op) => {
+                                            return `Assigned ${op.location} to ${op.target}`
+                                        });
                                         this.setState({
-                                            confirmation: true,
+                                            confirmation: d.operations.length ? success : null,
+                                            error: d.issues.length ? d.issues : null,
                                             parent: null,
                                             children: []
                                         });
